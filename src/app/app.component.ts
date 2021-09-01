5532697134dd2ec6c8a58cbbd965c049554b0530
Siddharth Patel <SiddharthP@contrado.com>
2021-09-01 17:55:25 +0530

    add other clause with select query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,41 +10,85 @@ import { AppService } from './app.service';
 export class AppComponent {
   title = 'atlan-app';
   public statement: string = '';
+  public filteredColumns: string[] = [];
   public queryData: ITable = {} as ITable;
   constructor(private appService: AppService) { }
 
   public excuteQuery(url: string) {
-    this.appService.excuteQuery(url).subscribe(data => this.queryData = data);
+    this.appService.excuteQuery(url).subscribe(data => {
+      this.queryData = data;
+      if (this.filteredColumns.length > 0) {
+        this.queryData.properties = this.filteredColumns;
+      }
+    });
   }
 
   public execute() {
+    this.clearSetup();
     const selectedStatement = this.getSelectedStatement().trim();
-    const url = this.createExecutableUrl(selectedStatement);
-    this.excuteQuery(url);
+    if (selectedStatement) {
+      const url = this.createExecutableUrl(selectedStatement);
+      this.excuteQuery(url);
+    }
   }
 
   private getSelectedStatement(): string {
-    return 'select * from posts';
+    let selectedText;
+    if (document.getSelection) {
+      selectedText = document.getSelection();
+    }
+    return selectedText && selectedText.toString() !== '' ? selectedText.toString() : this.statement;
   }
 
   private createExecutableUrl(statement: string): string {
     const splitStatement = statement.split(' ');
-    let url = `${this.getClause(splitStatement, Clause.FROM)}?`
+    let url = `${this.getClause(splitStatement, Clause.FROM)}`;
+    url = `${url}${this.appendQueryParams(url,this.getClause(splitStatement, Clause.WHERE))}`;
+    url = `${url}${this.appendQueryParams(url,this.getClause(splitStatement, Clause.LIKE))}`;
+    url = `${url}${this.appendQueryParams(url,this.getClause(splitStatement, Clause.LIMIT))}`;
+    url = `${url}${this.appendQueryParams(url,this.getClause(splitStatement, Clause.ORDERBY))}`;
+    this.getClause(splitStatement, Clause.SELECT);
     console.log('url:', url);
     return url;
   }
 
   private getClause(statement: string[], clause: string): string {
     switch (clause) {
-      case 'from':
+      case Clause.SELECT:
+        const colValue = this.getClauseValue(statement, clause);
+        if (colValue !== '*') {
+          this.filteredColumns = colValue.split(',').map(v => v.trim());
+        }
+        return colValue;
+      case Clause.LIKE:
+        const likeValue = this.getClauseValue(statement, clause);
+        return likeValue ? `q=${likeValue.replace(/%/ig, '').replace(/'/ig, '')}` : '';
+      case Clause.LIMIT:
+        const limitValue = this.getClauseValue(statement, clause);
+        return limitValue ? `_limit=${limitValue}` : '';
+        case Clause.ORDERBY:
+        const colName = this.getClauseValue(statement, clause, 2);
+        const orderValue = this.getClauseValue(statement, clause, 3);
+        return colName ? `_sort=${colName}&_order=${orderValue ?? 'asc'}` : '';
+      case Clause.FROM:
+      case Clause.WHERE:
         return this.getClauseValue(statement, clause);
       default:
         return '';
     }
   }
 
-  private getClauseValue(statement: string[], clause: string): string {
+  private getClauseValue(statement: string[], clause: string, clauseValueIndex = 1): string {
     const clauseIndex = statement.indexOf(clause);
-    return statement[clauseIndex + 1];
+    return clauseIndex === -1 ? '' : statement[clauseIndex + clauseValueIndex];
+  }
+
+  private appendQueryParams(url: string, queryString: string): string {
+    return queryString ? url.indexOf('?') === -1 ? `?${queryString}` : `&${queryString}` : '';
+  }
+
+  private clearSetup(): void {
+    this.queryData = {} as ITable;
+    this.filteredColumns = [];
   }
 }
--- src/app/app.helper.ts
+++ src/app/app.helper.ts
@@ -1,8 +1,13 @@
-export interface ITable{
+export interface ITable {
     properties: string[];
     records: any[];
 }
 
 export enum Clause {
+    SELECT = 'select',
     FROM = 'from',
+    WHERE = 'where',
+    LIKE = 'like',
+    LIMIT = 'limit',
+    ORDERBY = 'order'
 }
